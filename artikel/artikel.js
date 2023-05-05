4eb0406ef7d1955d68e4829af10dafd43c3ecf3c
unknown <dainnaxjakarta91@gmail.com>
2023-05-05 21:22:35 +0700

    mengganti tombol login dan daftar menjadi logout di navbar

--- artikel/artikel.js
+++ artikel/artikel.js
@@ -69,3 +69,13 @@ async function fetchData() {
 }
 
 fetchData();
+
+// ketika tombol logout di klik
+const logoutBtn = document.getElementById("logout-btn");
+
+logoutBtn.addEventListener("click", function () {
+  const confirmLogout = confirm("Apakah anda ingin keluar?");
+  if (confirmLogout) {
+    window.location.href = window.location.origin + "/index.html";
+  }
+});
